f2406c1bdc8d4a69418f782186b6f995eb85d83e
Darpit Makwana <darpitpatel2008@gmail.com>
2022-01-20 17:57:34 +0530

    Commit

---
+++ app.js
@@ -0,0 +1,45 @@
+var num1Input = document.getElementById("num1");
+var num2Input = document.getElementById("num2");
+var operator = document.getElementById("operator");
+var resultLable = document.getElementById("resultLable");
+var buttonElement = document.querySelector("button");
+function calculateResult(num1, num2, op) {
+    switch (op) {
+        case "+":
+            return num1 + num2;
+        case "-":
+            return num1 - num2;
+        case "*":
+            return num1 * num2;
+        case "/":
+            return num1 / num2;
+        default:
+            return "Invalide Operator, Please select valid operator.";
+    }
+}
+var OutputMode;
+(function (OutputMode) {
+    OutputMode[OutputMode["CONSOLE"] = 0] = "CONSOLE";
+    OutputMode[OutputMode["ALERT"] = 1] = "ALERT";
+})(OutputMode || (OutputMode = {}));
+function printResult(result, printMode) {
+    if (printMode === OutputMode.CONSOLE) {
+        console.log(result);
+        resultLable.innerHTML = "Result: ".concat(result);
+    }
+    else if (printMode === OutputMode.ALERT) {
+        alert(result);
+    }
+}
+buttonElement.addEventListener("click", function () {
+    var num1 = +num1Input.value;
+    var num2 = +num2Input.value;
+    var op = operator.value;
+    var result = calculateResult(num1, num2, op);
+    if (typeof result == "string") {
+        printResult(result, OutputMode.ALERT);
+    }
+    else {
+        printResult(result, OutputMode.CONSOLE);
+    }
+});
---
+++ app.ts
@@ -0,0 +1,46 @@
+const num1Input = document.getElementById("num1") as HTMLInputElement;
+const num2Input = <HTMLInputElement>document.getElementById("num2");
+const operator = document.getElementById("operator") as HTMLInputElement;
+const resultLable = document.getElementById("resultLable") as HTMLInputElement;
+const buttonElement = document.querySelector("button");
+
+function calculateResult(num1: number, num2: number, op: string) {
+  switch (op) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+    case "/":
+      return num1 / num2;
+    default:
+      return "Invalide Operator, Please select valid operator.";
+  }
+}
+
+enum OutputMode {
+  CONSOLE,
+  ALERT,
+}
+
+function printResult(result: string | number, printMode: OutputMode) {
+  if (printMode === OutputMode.CONSOLE) {
+    console.log(result);
+    resultLable.innerHTML = `Result: ${result}`;
+  } else if (printMode === OutputMode.ALERT) {
+    alert(result);
+  }
+}
+
+buttonElement.addEventListener("click", () => {
+  const num1 = +num1Input.value;
+  const num2 = +num2Input.value;
+  const op = operator.value;
+  const result: number | string = calculateResult(num1, num2, op);
+  if (typeof result == "string") {
+    printResult(result, OutputMode.ALERT);
+  } else {
+    printResult(result, OutputMode.CONSOLE);
+  }
+});
